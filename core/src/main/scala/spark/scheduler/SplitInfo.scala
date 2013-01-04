c4bdd0550ecf4df1577bd5109889655e9e896a07
Mridul Muralidharan <mridul@gmail.com>
2013-01-04 13:46:38 +0530

    Integrate review comments from Matei

---
+++ core/src/main/scala/spark/scheduler/SplitInfo.scala
@@ -0,0 +1,12 @@
+package spark.scheduler
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mridulm
+ * Date: 1/4/13
+ * Time: 12:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class SplitInfo {
+
+}
