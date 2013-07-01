6d8df918da4508040bc09283fbab70a6617a569f
root <root@ip-10-165-19-218.ec2.internal>
2013-07-01 02:45:00 +0000

    Fix performance bug with new Python code not using buffered streams

--- core/src/main/scala/spark/SparkEnv.scala
+++ core/src/main/scala/spark/SparkEnv.scala
@@ -54,7 +54,8 @@ class SparkEnv (
 
   def createPythonWorker(pythonExec: String, envVars: Map[String, String]): java.net.Socket = {
     synchronized {
-      pythonWorkers.getOrElseUpdate((pythonExec, envVars), new PythonWorkerFactory(pythonExec, envVars)).create()
+      val key = (pythonExec, envVars)
+      pythonWorkers.getOrElseUpdate(key, new PythonWorkerFactory(pythonExec, envVars)).create()
     }
   }
 }
