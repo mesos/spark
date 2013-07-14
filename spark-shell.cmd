7bbac420283c99b3153d3cea2cdbed185cc9a98c
Matei Zaharia <matei.zaharia@gmail.com>
2013-07-14 22:38:06 +0000

    Fixes to ADD_JARS on Windows

--- spark-shell.cmd
+++ spark-shell.cmd
@@ -1,9 +1,4 @@
 @echo off
 set FWDIR=%~dp0
 set SPARK_LAUNCH_WITH_SCALA=1
-if "x%ADD_JARS%"=="x" goto no_add_jars
-  echo Adding JARs to classpath: %ADD_JARS%
-  set TMP=%ADD_JARS:,=;%
-  set SPARK_CLASSPATH=%SPARK_CLASSPATH%;%TMP%
-:no_add_jars
 cmd /V /E /C %FWDIR%run2.cmd spark.repl.Main %*
